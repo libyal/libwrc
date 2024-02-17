441a9fb8db704c1d98ad2cd717e7c3a407657fc4
Joachim Metz <joachim.metz@gmail.com>
2024-02-17 07:43:51 +0100

    Applied updates

--- libwrc/libwrc_version_information_resource.c
+++ libwrc/libwrc_version_information_resource.c
@@ -1,7 +1,7 @@
 /*
  * String table (VERSIONINFO) resource functions
  *
- * Copyright (C) 2011-2023, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2024, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- libwrc/wrc_version_information_resource.h
+++ libwrc/wrc_version_information_resource.h
@@ -1,7 +1,7 @@
 /*
  * The Windows RC version information (VERSIONINFO) resource
  *
- * Copyright (C) 2011-2023, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2024, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
